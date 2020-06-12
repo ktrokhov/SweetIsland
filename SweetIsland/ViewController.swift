a067d60062b1f3595ccab56e3f9c4163077017fd
Trokhov Kirill <ktrokhov@gmail.com>
2020-06-12 08:33:31 +0300

    Initial Commit

---
+++ SweetIsland/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SweetIsland
+//
+//  Created by Kirill on 12.06.2020.
+//  Copyright © 2020 Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
